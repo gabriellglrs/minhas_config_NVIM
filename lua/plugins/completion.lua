38afc011e95dea2426274b06d6e4b01a0b0709b6
Gabriel Lucas Rodrigues Souza <gabriellucas.souza@inss.gov.br>
2026-09-01 10:37:57 -0300

    fix: Enter nao confirmava autocomplete (integracao cmp + autopairs + Tab)
    - adiciona nvim-autopairs.completion.cmp on_confirm_done para Enter funcionar
    - CR agora com behavior Replace e select=true
    - Tab confirma cmp ou expande luasnip, S-Tab volta no snippet
    - C-n/C-p com behavior Insert

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -15,6 +15,16 @@ return {
       local luasnip = require("luasnip")
       require("luasnip.loaders.from_vscode").lazy_load()
 
+      -- integra autopairs no confirm do cmp (fix Enter)
+      local ok_pair, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+      if ok_pair then cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done()) end
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -22,13 +32,31 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({ select = true })
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
