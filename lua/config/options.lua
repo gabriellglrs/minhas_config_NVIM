7959dea596d0707f3d424aa70755e454b595c8b7
GABRIEL LUCAS <119793695+gabriellglrs@users.noreply.github.com>
2026-09-10 12:22:35 -0300

    feat: adiciona atalho Home (Alpha) e clipboard WSL2

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -41,3 +41,16 @@ vim.opt.foldcolumn = "1"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
+
+-- WSL2 clipboard via win32yank
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+}
