ab5821f9f080baa545b5ba71726f2c0d6155a26a
GABRIEL LUCAS <119793695+gabriellglrs@users.noreply.github.com>
2026-09-10 12:41:33 -0300

    fix: move mykeymaps.txt pra raiz (doc/ causava erro vimdoc)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,11 +2,11 @@ local map = vim.keymap.set
 
 -- Comando para abrir guia de comandos
 vim.api.nvim_create_user_command("MyCommands", function()
-  vim.cmd("edit " .. vim.fn.stdpath("config") .. "/doc/mykeymaps.txt")
+  vim.cmd("edit " .. vim.fn.stdpath("config") .. "/mykeymaps.txt")
   vim.bo.filetype = "text"
 end, { desc = "Abrir guia de comandos" })
 vim.api.nvim_create_user_command("Mc", function()
-  vim.cmd("edit " .. vim.fn.stdpath("config") .. "/doc/mykeymaps.txt")
+  vim.cmd("edit " .. vim.fn.stdpath("config") .. "/mykeymaps.txt")
   vim.bo.filetype = "text"
 end, { desc = "Abrir guia de comandos (atalho)" })
 
